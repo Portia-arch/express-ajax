9092f8b2bdc418033292ea9300c7605eed5d3c4f
Portia-arch <portia.mkhabela@umuzi.org>
2020-05-09 21:23:29 +0200

    Addition for ajax

--- public/Javascript/index.js
+++ public/Javascript/index.js
@@ -1,32 +1,64 @@
-document.getElementById(myForm).addEventListener('submit', 'addNewVisitor');
+form.onsubmit = async (event) => {
+  event.preventDefault();
+
+  const body = {
+    name: event.target[0].value,
+    assistant: event.target[1].value,
+    age: event.target[2].value,
+    date: event.target[3].value,
+    time: event.target[4].value,
+    comment: event.target[5].value,
+  };
+
+  submit(body);
+
+  for (let i = 0; i < e.target.length; i++) {
+    e.target[i].value = null;
+  }
+};
+
+const submit = async (body) => {
+  const url = "http://127.0.0.1:3000/addNewVisitor";
+
+  const res = await fetch(url, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+
+  const data = await res.json();
+  createTableRows([data.visitors]);
+
+  return data;
+};
 
 // const addNewVisitor = async ()=> {
 
 // }
 
 //add a new visitor
-const newPost = async () => {
-    let newForm = document.getElementById('myForm');
-    let newbody = document.getElementById('body');
+// const newPost = async () => {
+//     let newForm = document.getElementById('myForm');
+//     let newbody = document.getElementById('body');
 
-  const options = {
-    method: 'POST',
-    body: JSON.stringify(newForm:myForm, newbody:body),
-    headers: new Headers({
-      'Content-Type': 'application/json'
-    })
-  }
-  return fetch(`http://localhost:3001/single-page-app`, options)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(error => console.log(`Error: ${Error}`))
-  });
+//   const options = {
+//     method: 'POST',
+//     body: JSON.stringify(newForm:myForm, newbody:body),
+//     headers: new Headers({
+//       'Content-Type': 'application/json'
+//     })
+//   }
+//   return fetch(`http://localhost:3001/single-page-app`, options)
+//     .then(res => res.json())
+//     .then(res => console.log(res))
+//     .catch(error => console.log(`Error: ${Error}`))
+//   };
 
     // const data = await form.json();
     // createTableRows([data.visitor]);
 
     // return data;
-}
+
 
 //list all the visitors
 // const viewVisitors = async ()=> {
